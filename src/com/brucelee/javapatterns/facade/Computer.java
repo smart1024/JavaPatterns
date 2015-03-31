7b8014ecb2e4d3c2ce957c4b7e9de014fee5200b
robertlee <1059232202@qq.com>
2015-03-31 10:33:13 +0800

    补充README文件

--- src/com/brucelee/javapatterns/facade/Computer.java
+++ src/com/brucelee/javapatterns/facade/Computer.java
@@ -9,7 +9,7 @@ package com.brucelee.javapatterns.facade;
 
 /** 
  * @ClassName: Computer 
- * @Description: 装饰类：Computer跟其他三者是聚合关系
+ * @Description: 装饰类：Computer跟其他三者是聚合关系，观模式是为了解决类与类之家的依赖关系的(计算机的例子)
  * @author robertlee1059@163.com 
  * @date 2015-3-30 上午11:48:14 
  *  
